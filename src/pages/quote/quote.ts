7060e4acc27f6ef53e389d56d5368af10c8946ea
adrianplusplus <adrian.galicia92@gmail.com>
2018-01-13 16:47:17 -0300

    Adding Custom Styles(Section 3, Lesson 51)

--- src/pages/quote/quote.ts
+++ src/pages/quote/quote.ts
@@ -1,13 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the QuotePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-quote',
@@ -15,12 +8,6 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class QuotePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad QuotePage');
-  }
 
   onAddToFavorite(){
 
