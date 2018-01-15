09bb708fe8c1abda0172da7595741a15bb295c14
adrianplusplus <adrian.galicia92@gmail.com>
2018-01-15 11:20:12 -0300

    How to change the Root Page(Section 3, Lesson 69)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import {Component, ViewChild} from '@angular/core';
+import {MenuController, NavController, Platform} from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -12,8 +12,12 @@ import {SettingsPage} from "../pages/settings/settings";
 export class MyApp {
   tabsPage = TabsPage;
   settingsPage=SettingsPage;
+  @ViewChild('nav') nav:NavController;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform,
+              statusBar: StatusBar,
+              splashScreen: SplashScreen,
+              private menuCtrl:MenuController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -23,7 +27,8 @@ export class MyApp {
   }
 
   onLoad(page:any){
-
+    this.nav.setRoot(page);
+    this.menuCtrl.close();
   }
 }
 
